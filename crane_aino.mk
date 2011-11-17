4e5a8b9bbc39f8d4969d95a48c762a4992fda151
zhangxu <zhangxu@allwinnertech.com>
2011-11-17 13:12:12 +0800

    add copy command

--- crane_aino.mk
+++ crane_aino.mk
@@ -6,7 +6,8 @@ PRODUCT_COPY_FILES += \
 	device/softwinner/crane-aino/ueventd.sun4i.rc:root/ueventd.sun4i.rc \
 	device/softwinner/crane-aino/init.sun4i.rc:root/init.sun4i.rc \
 	device/softwinner/crane-aino/sun4i-keyboard.kl:system/usr/keylayout/sun4i-keyboard.kl \
-	device/softwinner/crane-aino/hv_keypad.kl:system/usr/keylayout/hv_keypad.kl
+	device/softwinner/crane-aino/hv_keypad.kl:system/usr/keylayout/hv_keypad.kl \
+	device/softwinner/crane-aino/gt80x.idc:system/usr/idc/gt80x.idc
 	
 PRODUCT_COPY_FILES += \
 	device/softwinner/crane-aino/initlogo.rle:root/initlogo.rle
