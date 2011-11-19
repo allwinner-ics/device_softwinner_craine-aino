8d94efb137d85bab6bd9b746eb89ad1a147612b8
benwu <benwu@allwinnertech.com>
2011-11-19 17:54:58 +0800

    add gsensor bma250 support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,3 +4,12 @@
 #
 
 include device/softwinner/crane-common/BoardConfigCommon.mk
+
+# Gsensor board config
+# we can use this string : mma7660, mxc622x , bma250
+SW_BOARD_USES_GSENSOR_TYPE := bma250
+BOARD_GSENSOR_DIRECT_X := false
+BOARD_GSENSOR_DIRECT_Y := true
+BOARD_GSENSOR_DIRECT_Z := true
+
+BOARD_GSENSOR_XY_REVERT := true
