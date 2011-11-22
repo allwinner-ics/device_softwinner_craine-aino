ee2d79778bcd0dc936d7ac9caf3837d22a3d33fa
weiziheng <weiziheng@allwinnertech.com>
2011-11-22 19:30:31 +0800

    add nano wifi support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,11 @@ BOARD_GSENSOR_DIRECT_Y := true
 BOARD_GSENSOR_DIRECT_Z := true
 
 BOARD_GSENSOR_XY_REVERT := true
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+
+# Wifi chipset select
+# usb wifi "rtl8192cu"; sdio wifi "nanowifi"/"ar6302"/"usibcm4329"
+SW_BOARD_USR_WIFI := nanowifi
